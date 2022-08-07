512ae7049f461dc3ed10bd7c8ddfd05c83c41b56
rybkin <erybkin@yandex.ru>
2022-08-07 21:54:12 +0700

    add Homework07_chet_1_N

---
+++ Homework07_chet_1_N/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//   5 -> 2, 4
+//   8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= number1)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+      //  Console.Write(", ");
+    }
+i++;
+}
