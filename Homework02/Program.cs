8cf0acd002f2c26efb22f6aa59e69d7fa4ffaab9
rybkin <erybkin@yandex.ru>
2022-08-07 13:20:37 +0700

    add homework02

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number2);
+}
