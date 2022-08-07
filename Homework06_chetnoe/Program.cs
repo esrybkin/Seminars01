752622b77c609144733c8d6e77169dee51491d6b
rybkin <erybkin@yandex.ru>
2022-08-07 21:35:57 +0700

    add Homework06_chetnoe

---
+++ Homework06_chetnoe/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+   Console.Write("Число нечетноё"); 
+}
