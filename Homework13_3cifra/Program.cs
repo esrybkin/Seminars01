e36ff9c616eb944c1001c0347b03bfce019d8132
rybkin <erybkin@yandex.ru>
2022-08-13 12:52:30 +0700

    fix example13

--- Homework13_3cifra/Program.cs
+++ Homework13_3cifra/Program.cs
@@ -5,12 +5,16 @@
 
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-if (((a / 100) % 10) == 0)
+if (a / 100  == 0)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else
 {
-    int b = a % 10;
-    Console.WriteLine(b);
+    while (a / 1000 != 0)
+    {
+    a = a / 10;
+    }
+    a = a % 10;
+    Console.WriteLine(a);
 }
