9ab0781e0a0747030eb9a0ec1942f15ef3859dca
rybkin <erybkin@yandex.ru>
2022-08-13 12:02:02 +0700

    fix minor examples

--- Homework15_Weakend/Program.cs
+++ Homework15_Weakend/Program.cs
@@ -1,4 +1,8 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//  6 -> да
+//  7 -> да
+//  1 -> нет
+
 Console.WriteLine("Введите цифру");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a < 1 || a > 7)
@@ -7,10 +11,10 @@ if (a < 1 || a > 7)
 }
 else if (a >= 1 & a < 6)
 {
-    Console.WriteLine("Это будний день");
+    Console.WriteLine("Нет");
 }
 
 else if (a >= 6 & a < 8)
 {
-    Console.WriteLine("Это выходной день");
+    Console.WriteLine("Да");
 }
