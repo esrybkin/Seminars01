835c67a6b2776a2cfb2390758296441542260aaf
rybkin <erybkin@yandex.ru>
2022-08-14 23:33:52 +0700

    add homework 19 and 21

---
+++ Homework19_Polindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+number1 = Math.Abs(number1);
+int i = 1;
+
+while (i <= number1)
+{
+    Console.WriteLine(i*i*i);
+i++;
+}
