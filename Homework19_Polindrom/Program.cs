035530b36304a6587ad3f39944c98aeac3475876
rybkin <erybkin@yandex.ru>
2022-08-16 19:10:54 +0700

    add homework

--- Homework19_Polindrom/Program.cs
+++ Homework19_Polindrom/Program.cs
@@ -4,13 +4,20 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-number1 = Math.Abs(number1);
-int i = 1;
+Console.WriteLine("Введите число: ");
+string? number1 = Console.ReadLine();
+string number2 = string.Empty;
 
-while (i <= number1)
+for (int i = 0; i < number1.Length; i++)
 {
-    Console.WriteLine(i*i*i);
-i++;
+   number2 = number2 + number1[number1.Length - 1 - i];
+}
+
+if (string.Compare(number1, number2) == 0)
+{
+    Console.WriteLine("Число палиндром");
+}
+else
+{
+    Console.WriteLine("Число не палиндром");
 }
