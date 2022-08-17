ac4481cf8886afaa01b7832c2412323d9a7b80d2
rybkin <erybkin@yandex.ru>
2022-08-17 22:54:27 +0700

    add homework27

---
+++ Homework27_sum_cifer_in_chislo/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+
+int Cycle(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }    
+    return sum;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Cycle(n));
---
+++ Seminar26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+
+int Cycle(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        n = n / 10;
+        sum += 1;
+    }    
+    return sum;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Cycle(n));
