5fa5405ff5424068631308a8022cdda49511e4a8
rybkin <erybkin@yandex.ru>
2022-08-17 23:53:06 +0700

    add homework29

---
+++ Homework28_proizv_chisel_mass/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28*: Напишите программу, в которой задается массив чисел(ввод может быть с клавиатуры, либо можете сами задавать массив чисел) и выдаёт произведение чисел от 1 до N, каждого элемента этого массива.
+// {2, 3, 4} -> 2, 6, 24
+// {5, 3, 6, 2, 5, 1} -> 6, 720, 2, 120, 1
+
+Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int [length];
+int[] arr2 = new int[length];
+int count = 0;
+for (int i=0; i<length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+while (count <= length)
+{
+    for (int k=0; k<arr[count]; k++)
+    {
+        arr2[k] = arr2[k] * 
+    }
+}
+
+
+Console.Write("[");
+Console.Write(String.Join(", ", arr));
+Console.Write("]");
---
+++ Homework29_zapolnite_array/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int length = 8;
+int[] arr = new int [length];
+for (int i=0; i<length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("[");
+Console.Write(String.Join(", ", arr));
+Console.Write("]");
