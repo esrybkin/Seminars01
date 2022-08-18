33f38392cc2b5d49645d1aee971557a7166536e6
rybkin <erybkin@yandex.ru>
2022-08-18 19:58:21 +0700

    add seminar30

---
+++ Seminar30_random_array_ot_0_do_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+ 
+Console.Clear();
+int n = 8;
+int min = 0;
+int max = 1;
+
+int[] Array (int n, int min, int max)
+{
+    int[] arr = new int[n];
+    for (int i=0; i<n; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+Console.Write("[");
+Console.Write(String.Join(", ", Array(n, min, max)));
+Console.Write("]");
+
