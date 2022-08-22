ff618352dcd13ddd349c0ec4f9037badbe8d568a
rybkin <erybkin@yandex.ru>
2022-08-22 18:25:07 +0700

    add homework34

---
+++ Homework34_kol-vo_chet_chisel/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] FillArray(int leng, int min, int max)
+{
+    Random rand = new Random();
+    int[] arr = new int[leng];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);     
+    }
+    return arr;
+}
+
+int[] array = FillArray(4, 100, 999);
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine('['+ string.Join(",", array)+']');
+Console.WriteLine($"Количество чётных чисел в массиве равно {count}");
