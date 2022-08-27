a5a1498149ba7b53ecdadd6705d8691cc90fbf6d
rybkin <erybkin@yandex.ru>
2022-08-27 15:58:45 +0700

    add hw44

--- Homework44/Program.cs
+++ Homework44/Program.cs
@@ -2,3 +2,15 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] fib = new int[n];
+fib[0] = 0;
+fib[1] = 1;
+for (int i=2; i<n; i++)
+{
+    fib[i] = fib[i-1] + fib[i-2];
+}
+
+Console.WriteLine('[' + string.Join(',', fib) + ']');
