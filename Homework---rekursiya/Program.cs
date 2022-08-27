0a86e41c5e76ba2bbaf2e28adfe6bd23e39d1e36
rybkin <erybkin@yandex.ru>
2022-08-27 16:23:39 +0700

    add zadacha rekursiya

--- Homework---rekursiya/Program.cs
+++ Homework---rekursiya/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N. (использовать рекурсию)
 // 4 -> 24
 // 5 -> 120
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Factor(int n)
+{
+  if (n == 1) return 1;
+  else return n * Factor(n-1);
+}
+
+Console.WriteLine($"Произведение чисел равно {Factor(n)}");
+
+
