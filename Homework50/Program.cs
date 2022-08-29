91e4179645d40582193e07b1d15ec78e89fa6c03
rybkin <erybkin@yandex.ru>
2022-08-29 18:13:30 +0700

    add hw 47,50,52

---
+++ Homework50/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+int m = 4;  //строки
+int n = 3;  //столбцы
+
+int[,] array = new int[m, n];
+Random random = new Random();
+for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            array[i, j] = random.Next(0, 10);
+           Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+Console.WriteLine("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.WriteLine("Введите номер столбца: ");
+int column = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (row > array.GetLength(0) || column > array.GetLength(1))
+{
+    Console.WriteLine("Такой позиции в массиве нет");
+}
+else
+{
+    Console.WriteLine(array[row, column]);
+}
---
+++ Homework52/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+int rows = 3;
+int columns = 4;
+Random random = new Random();
+
+int[,] FillArray(int rows,int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+            array[i,j] = random.Next(0,11);
+            Console.Write("{0,7:N0}", array[i,j]);
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int[,] arr = new int[rows, columns];
+arr = FillArray(rows,columns);
+double[] sum = new double[columns];
+for (int i=0; i<columns; i++)
+    {
+        for (int j=0; j<rows; j++)
+        {
+            sum[i] = sum[i] + arr[j,i];  
+        }
+        sum[i] = sum[i]/rows;     
+        Console.Write($"Среднее арифметическое {i+1} столбца: ");
+        Console.WriteLine("{0,7:N1}", sum[i]);        
+    }
